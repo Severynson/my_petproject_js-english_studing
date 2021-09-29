8d4ab01a056267918ec4ca7ec967407bd2db0a06
severyn <severynson.kurach@gmail.com>
2021-09-29 12:53:58 +0300

    changingButton'sFunctionWorking

--- 3) First test/firstTest.js
+++ 3) First test/firstTest.js
@@ -1,6 +1,9 @@
 'use strict';
+// import whichThemWasChoosen from '/2) choosingActions/choosingAction.js';
+// console.log(whichThemWasChoosen);
+
 let GoButton = document.getElementById('check-button');
-let question = document.getElementById('input');
+let inputWord = document.getElementById('input');
 let exampleWord = document.getElementById('example-word');
 
 function readTextFile(file, callback) {
@@ -25,15 +28,50 @@ readTextFile('words.json', function (text) {
   ////Exercise inside because of asyncronus////
   /////////////////////////////////////////////
 
-  console.log(question);
-  GoButton.addEventListener('click', e => {
-    exampleWord.textContent = 'changed now';
-    e.preventDefault();
-    console.log('ssss');
-    console.log(question);
-    question.value = 'ref';
-  });
-  /////////////////////////////////////////////
-  /////////////////////////////////////////////
-  /////////////////////////////////////////////
+  const foods = Object.entries(data.food);
+  console.log(foods);
+
+  GoButton.addEventListener('click', whatToCall);
+  let pairOfWords = 0;
+  exampleWord.textContent = foods[pairOfWords][1];
+
+  let mistakesInOneWord = 0;
+
+  function whatToCall() {
+    if (mistakesInOneWord === 0) {
+      ifCorrectThanNext();
+    } else if (mistakesInOneWord === 1) {
+      console.log('That is second time:');
+      secondMistake();
+    }
+  }
+  function ifCorrectThanNext() {
+    if (inputWord.value == foods[pairOfWords][0]) {
+      console.log('true!');
+      pairOfWords++;
+      exampleWord.textContent = foods[pairOfWords][1];
+    } else if (inputWord.value !== foods[pairOfWords][0]) {
+      console.log(
+        `You are stupid, not correct!!! First letter of the correct word was: ${foods[pairOfWords][0]}`
+      );
+      mistakesInOneWord++;
+    } else {
+      return;
+    }
+  }
+
+  function secondMistake() {
+    if (inputWord.value == foods[pairOfWords][0]) {
+      console.log('true!');
+      pairOfWords++;
+      exampleWord.textContent = foods[pairOfWords][1];
+      mistakesInOneWord = 0;
+    } else if (inputWord.value !== foods[pairOfWords][0]) {
+      console.log(
+        `You are stupid, not correct!!! First three leters of the correct word was: ${foods[pairOfWords][0]}`
+      );
+    }
+  }
 });
+
+///////////////////////////////////////////////////
